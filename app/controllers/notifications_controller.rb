41f115b67685d808e84ec2ca0f2b6a072dd83597
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-07-04 06:36:15 -0400

    actually using whoops logger

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def create
     details = YAML.load(params[:details].strip)
-    WhoopsNotifier.notify(:custom, details)
+    WhoopsLogger.log(:custom, details)
     render :action => :new
   end
 end
